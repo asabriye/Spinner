9f31d3aa930f8d7cb2730df8bf272e6f929ac276
asabriye <abdullahisabriye@gmail.com>
2021-11-03 02:18:53 +0000

    updated spinner

--- spinner1.js
+++ spinner1.js
@@ -17,3 +17,23 @@ setTimeout(() => {
   process.stdout.write('\r\\   '); 
 }, 700);
 
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1400);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1600);
+
+setTimeout(() => {
+  process.stdout.write('\r|   \n');
+}, 1800);
